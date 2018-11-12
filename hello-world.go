90082ee4393870e894cfc5e28a69fb1c289a6c87
direwolf91 <souravshrija91@gmail.com>
2018-11-12 22:08:15 +0530

    checking channels in go

--- hello-world.go
+++ hello-world.go
@@ -1,5 +1,20 @@
 package main
 import "fmt"
+import "time"
+
+func worker(done chan bool) {
+    fmt.Print("working...")
+    time.Sleep(time.Second)
+    fmt.Println("done")
+    done <- true
+}
+
+
 func main() {
-    fmt.Println("hello world")
+	var array_temp [2]int
+	done := make(chan bool, 1)
+	go worker(done)
+	<- done
+	fmt.Println("emp:", array_temp)
+	fmt.Println("hello world")
 }
